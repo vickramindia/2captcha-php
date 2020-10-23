5244876adac623c8756189d8461a2922eeb04aaf
vickramindia <73355240+vickramindia@users.noreply.github.com>
2020-10-23 21:40:40 +0530

    Update recaptcha_v3.php

--- examples/recaptcha_v3.php
+++ examples/recaptcha_v3.php
@@ -4,8 +4,7 @@ set_time_limit(610);
 
 require(__DIR__ . '/../src/autoloader.php');
 
-$solver = new \TwoCaptcha\TwoCaptcha('YOUR_API_KEY');
-
+$solver = new \TwoCaptcha\TwoCaptcha('fea4eebc4b9a7aad11a65fbdf813dffa');
 try {
     $result = $solver->recaptcha([
         'sitekey' => '6Le-wvkSVVABCPBMRTvw0Q4Muexq1bi0DJwx_mJ-',
